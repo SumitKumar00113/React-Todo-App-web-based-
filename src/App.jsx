f8171e6e5e4ce14255f72f30f46a7e35d274190d
SumitKumar <sumitmahaseth02@gmail.com>
2025-09-09 20:56:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Create from "./components/Create";
 import Read from "./components/Read";
+import Wrapper from "./context/Wrapper";
 
 const App = () => {
   return (
     <>
+    <Wrapper>
       <div className="w-screen h-screen bg-slate-700 flex p-10">
         <Create />
         <Read  />
       </div>
+      </Wrapper>
     </>
   );
 };
